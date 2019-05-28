d0fb3e810592a93bff08e3bee574f04b7156269c
Jan Tattermusch <jtattermusch@google.com>
2019-05-28 16:35:11 +0200

    add dotnet global tool stub

---
+++ src/dotnet-grpc/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace dotnet_grpc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("gRPC dotnet global tool not implemented yet!");
+            Environment.Exit(1);
+        }
+    }
+}
